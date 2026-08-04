be0504763629825508b1e26f243ebe462328b42c
AlexanderVukolov <vukolov5566@gmail.com>
2026-08-05 00:57:48 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-bloggers-github-v67-cabinet-data";
+const CACHE_NAME = "nsl-bloggers-github-v68-resilient-shared-state";
 const APP_ROOT = new URL("./", self.registration.scope).pathname;
 const APP_SHELL = [
   APP_ROOT,
