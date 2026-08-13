439a2f4345744b488ca517e05e35c18f5654c4e6
AlexanderVukolov <vukolov5566@gmail.com>
2026-08-13 20:18:19 +1000

    Исправить сохранение и счётчик блогеров

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-bloggers-github-v83-placement-summary-parity";
+const CACHE_NAME = "nsl-bloggers-github-v84-live-card-counter";
 const APP_ROOT = new URL("./", self.registration.scope).pathname;
 const APP_SHELL = [
   APP_ROOT,
