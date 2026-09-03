7e05878edd56ac22756e7a2210626e258c6bf21a
AlexanderVukolov <vukolov5566@gmail.com>
2026-09-03 21:11:47 +1000

    Обновить кэш приложения до v113

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-bloggers-github-v112-placement-guarantee-from-card";
+const CACHE_NAME = "nsl-bloggers-github-v113-actual-reach-blogger-search";
 const APP_ROOT = new URL("./", self.registration.scope).pathname;
 const APP_SHELL = [
   APP_ROOT,
